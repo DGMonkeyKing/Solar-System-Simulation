2667d7943116610258b517486fa4ab4d936812e1
DGMonkeyKing <davidnew33@gmail.com>
2016-03-18 02:32:54 +0100

    Added basic funtionality of stellarship
    Replacing the stellarship, an object has been included. The main
    functionality, like the key events and the chasing camera, is done.
    Optimizations must be done.

---
+++ Assets/Scripts/Controllers.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent(typeof(CharacterController))]
+
+public class Controllers : MonoBehaviour {
+
+	public float moveSpeed = 0f;
+	public float turnSpeed = 20f;
+
+	private float constSpeed = 2f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Rotate (Vector3.forward * -turnSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Rotate (Vector3.forward * turnSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (Vector3.right * -turnSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (Vector3.right * turnSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			if (moveSpeed < 240f) {
+				moveSpeed += constSpeed;
+				constSpeed += 2f;
+			} else
+				moveSpeed = 240f;
+		} else {
+			if (moveSpeed > 0f) {
+				moveSpeed -= constSpeed;
+				constSpeed -= 2f;
+			} else
+				moveSpeed = 0f;
+		}
+		transform.Translate (Vector3.up * moveSpeed * Time.deltaTime);
+	}
+}
