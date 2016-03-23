49b331aeef8e4aace514b526b1800d13dbeeec81
DGMonkeyKing <davidnew33@gmail.com>
2016-03-23 18:38:40 +0100

    Added the first 2 models
    Has been added the 2 first 3DModels belonging to Star Wars and Alien
    universe.
    In addition, the scripts has been upload to allow the stellarship to
    have animation, like the rotating-head R2 or the wide open wings of
    Xwing

--- Assets/Scripts/Controllers.cs
+++ Assets/Scripts/Controllers.cs
@@ -7,42 +7,47 @@ public class Controllers : MonoBehaviour {
 
 	public float moveSpeed = 0f;
 	public float turnSpeed = 20f;
-
+	public Animator anim;
 	private float constSpeed = 2f;
 
 	// Use this for initialization
 	void Start () 
 	{
+		anim.enabled = false;
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			transform.Rotate (Vector3.forward * -turnSpeed * Time.deltaTime);
-		}
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			transform.Rotate (Vector3.forward * turnSpeed * Time.deltaTime);
-		}
-		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.Rotate (Vector3.right * -turnSpeed * Time.deltaTime);
 		}
-		if (Input.GetKey (KeyCode.RightArrow)) {
+		if (Input.GetKey (KeyCode.DownArrow)) {
 			transform.Rotate (Vector3.right * turnSpeed * Time.deltaTime);
 		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (Vector3.up * -turnSpeed * Time.deltaTime);
+			transform.Rotate (Vector3.forward * -turnSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (Vector3.up * turnSpeed * Time.deltaTime);
+			transform.Rotate (Vector3.forward * turnSpeed * Time.deltaTime);
+		}
 		if (Input.GetKey (KeyCode.Space)) {
+			anim.enabled = true;
 			if (moveSpeed < 240f) {
 				moveSpeed += constSpeed;
 				constSpeed += 2f;
 			} else
 				moveSpeed = 240f;
 		} else {
+			anim.enabled = false;
 			if (moveSpeed > 0f) {
 				moveSpeed -= constSpeed;
 				constSpeed -= 2f;
 			} else
 				moveSpeed = 0f;
 		}
-		transform.Translate (Vector3.up * moveSpeed * Time.deltaTime);
+		transform.Translate (Vector3.back * moveSpeed * Time.deltaTime);
 	}
 }
